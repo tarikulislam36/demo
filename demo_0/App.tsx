c374e26e7d2d36aa1447c36b267a33be98b13726
tarikulislam <tarikulislam4145@gmail.com>
2024-03-29 23:40:48 +0530

    in

---
+++ demo_0/App.tsx
@@ -0,0 +1,23 @@
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import React from 'react'
+import Head from './components/Head';
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+      <Head> </Head>
+    </View>
+  )
+}
+
+export default App
---
+++ demo_0/components/Head.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class Head extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Head</Text>
+      </View>
+    )
+  }
+}
+
+export default Head
